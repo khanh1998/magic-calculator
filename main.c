354be6377a0e2f675c6b7e71d049a183cf70fded
khanh1998 <kkhanh_2012@yahoo.com>
2016-12-22 22:11:52 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-Node* creatNode(char c);
-void push (char c, Node** pHead); //them mot node vao stack
-char pop (Node** pHead); //lay ky tu dau tien cua stack
-typedef struct node Node;
-struct node{
-	char data;
-	Node next;
-}
 
 void printREADME() {
     printf("Welcome to MAGIC CALCULATOR.");
@@ -16,25 +6,6 @@ void printREADME() {
 
 int main(void) {
     printREADME();
-    Node *Head_sh = creatNode(' '); //stack chua toan hang
-	Node *Head_st = creatNode(' '); //stack chua toan tu
+
     return 0;
-}
-void push (char c, Node** pHead){
-	//tao them mot node chua ky tu c
-	Node* p = creatNode(c);
-	//day node vao stack
-	
-}
-Node* creatNode(char c){
-	Node *p = NULL;
-	p = malloc(sizeof(Node));
-	if (p == NULL){
-		exit(0);
-	}
-	else {
-		p->data = c; //(*p).data = c;
-		p->Next = NULL;
-	}
-	return p;
 }
